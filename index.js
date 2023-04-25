452fdbcee8ce610544d58cd27dccd9c1d92cde93
liciene <ljcruz7@gmail.com>
2023-04-25 08:47:59 -0300

    initializing project

---
+++ index.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Calculadora";
+
+
+/* const calmat = (a, b) => {
+    const result = a + b;
+    console.log(result)
+} */
+
+const titulo = document.getElementById("myHeading");
+document.write('<h1 style="color: #025fff">' + titulo.innerText + ' agora em azul </h1>');
